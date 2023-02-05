3c69dc15515edcde07683685afbfa4f7990649d5
Nehotieva <nekhotiaeva.a@gmail.com>
2023-02-05 18:34:52 +0200

    first commit

---
+++ practice 1/practise1.js
@@ -0,0 +1,23 @@
+// Hello World!
+
+let firstVariable;
+
+firstVariable = "Hello World";
+
+console.log(firstVariable);
+
+let userName = "Anastasia";
+
+console.log(userName);
+
+let width, height, length;
+
+let button;
+let form;
+let input;
+
+let childAge = 12;
+childAge = 14;
+console.log(childAge);
+
+
